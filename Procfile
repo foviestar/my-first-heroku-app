8469391403849f7b88e06f13286240ce53e4b304
McLaughlin <Thurman.McLaughlin8@T-Mobile.com>
2018-08-16 10:57:12 -0700

    'mymessage'

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python firstscript.py $PORT
+web: python firstscript.py
--- firstscript.py
+++ firstscript.py
@@ -1,1 +1,8 @@
-print('success')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def source():
+ html = 'Hello World!'
+ return html
